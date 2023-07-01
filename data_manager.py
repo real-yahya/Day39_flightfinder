ff4e6abe24e612a9210d229c4d1e60b3d89c823f
yah.mohamed03 <yah.mohamed03@gmail.com>
2023-07-01 21:32:27 +0100

    Step 2 complete but not fully functional.

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,23 @@
+import requests
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.apiURL = "https://api.sheety.co/ae6e597461e60d80ebb76f45afe11f2a/flightDeals/prices"
+        self.data = {}
+
+    def read_sheet(self):
+        response = requests.get(self.apiURL)
+        self.data = response.json()
+        return self.data
+
+    def update_iata(self):
+        for city in self.data[0]['prices']:
+            iata = {
+                'price': {
+                    'iataCode': city['iataCode'],
+                }
+            }
+            response = requests.put(
+                f"{self.apiURL}/{city['id']}", json=iata)
+            print(response.text)
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+from flight_search import FlightSearch
+from pprint import pprint
+
+sheets = DataManager()
+scanner = FlightSearch()
+sheet_data = [sheets.read_sheet()]
+
+sheets.data = sheet_data
+sheets.update_iata()
